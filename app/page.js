4dfad895462c431c0d427533c01dadc028827e95
Md Econozzaman Econ <92664558+ezecon@users.noreply.github.com>
2026-06-06 23:01:24 +0600

    first commit

--- app/page.js
+++ app/page.js
@@ -1,11 +1,5 @@
-'use client'
-import { useState, useEffect } from 'react'
-import { useRouter } from 'next/router';
 export default function Home() {
-  const router = useRouter();
-  useEffect(() => {
-    router.push('/dashboard')
-  }, [])
+
   return (
     <div
       className="
