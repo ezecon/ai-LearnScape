1bbd08b713e149b99c901f2a9b5f8aa46bc21d05
Md Econozzaman Econ <92664558+ezecon@users.noreply.github.com>
2026-06-06 23:00:06 +0600

    first commit

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router';
 export default function Home() {
   const router = useRouter();
